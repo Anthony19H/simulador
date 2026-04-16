68724b3aac03beaa02fc081a2c0b03993f371e94
Anthony19H <anthony.fabian.herrera@hotmail.com>
2026-04-15 21:30:49 -0500

    Fcuncion calcular,calcularDisponible,calcularCapacidad de pago

--- funciones.js
+++ funciones.js
@@ -1,1 +1,20 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+ function calcularDisponible(ingresos, egresos) {
+    let valorDisponible=ingresos-egresos;
+    if(valorDisponible<0){
+        return 0;
+    }
+    return valorDisponible;
+}
+
+function calcularCapacidadPago(montoDisponible){
+    let capacidadPago=montoDisponible/2;
+    return capacidadPago;
+
+
+}
+
+function mostrarEnSpan(idSpan,valor){
+    let componente=document.getElementById(idSpan);
+    componente.textContent=valor;
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,17 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+
+function calcular(){
+
+    let ingresos=parseFloat(document.getElementById("txtIngresos").value);
+    let egresos=parseFloat(document.getElementById("txtEgresos").value);
+    let disponible=calcularDisponible(ingresos,egresos);
+    
+    let disponibleFormateado = disponible.toFixed(2);
+    
+    mostrarEnSpan("spnDisponible", disponibleFormateado);
+    
+    let capacidadPago = calcularCapacidadPago(disponibleFormateado);
+
+    mostrarEnSpan("spnCapacidadPago",capacidadPago);
+}
+
